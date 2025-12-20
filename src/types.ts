402c721ed02f3d0562d8a802ae3ac41dc5dd5be1
Claudiasals <salsiniclaudia@gmail.com>
2025-12-20 18:36:18 +0100

    creo l interfaccia todo

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+/* TypeScript
+Creare un'Interfaccia Todo
+Nella directory src, crea un nuovo file chiamato types.ts. 
+Definisci un'interfaccia Todo con le seguenti proprietà: 
+- id (number), 
+- title (string), 
+- completed (boolean). */
+
+// Scrivo la forma dell'oggetto in ts
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
