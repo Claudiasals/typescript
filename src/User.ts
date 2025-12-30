19bb87f55aa279a58062d31f382c75eb675e261a
Claudiasals <salsiniclaudia@gmail.com>
2025-12-30 16:43:44 +0100

    metodo per aggiungere un Todo all'utente

--- src/User.ts
+++ src/User.ts
@@ -1,13 +1,20 @@
-export class User {
-    id: number;
-    name: string;
-    email?: string; // "?" = proprietà opzionale
+import type { Todo } from './types.js'; // importa il tipo Todo
 
-    constructor(id: number, name: string, email?: string) {
-        this.id = id;
-        this.name = name;
-        if (email) {
-            this.email = email;
-        }
-    }
+export class User {
+  id: number;
+  name: string;
+  email?: string;
+  todos: Todo[]; // array dei Todo dell'utente
+
+  constructor(id: number, name: string, email?: string) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.todos = []; // inizializziamo l'array vuoto
+  }
+
+  // metodo per aggiungere un Todo all'utente
+  addTodo(todo: Todo): void {
+    this.todos.push(todo);
+  }
 }
